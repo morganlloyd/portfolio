2cf617278d658dfc9f0b3e4382ddbdd178780c56
Morgan <55657946+morganlloyd@users.noreply.github.com>
2022-12-14 21:10:45 -0700

    added javascript logic for mobile menu

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const app = {};
 
 app.init = function () {
-
+    app.menu();
+    app.noMenu();
 }
 
-// slide out menu functions
+// SLIDE OUT MENU
 
 // grab the elements 
+app.menuOpen = document.querySelector('#menuOpen');
+app.menuClose = document.querySelector('#menuClose');
+app.mobileMenu = document.querySelector('.slideOutMenu');
+
+// open menu
+app.menu = function () {
+    app.menuOpen.addEventListener('click', () => {
+        app.mobileMenu.classList.add('open');
+    })
+}
+
+// close menu
+app.noMenu = function () {
+    app.menuClose.addEventListener('click', () => {
+        app.mobileMenu.classList.remove('open');
+    });
+    
+}
 app.init();
